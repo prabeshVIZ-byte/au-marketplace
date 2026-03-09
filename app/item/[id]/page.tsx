e7accb2d54f61d26364d2cc2e6b05ad37c3ed342
prabeshVIZ-byte <pkhadkahere@gmail.com>
2026-03-09 02:46:00 -0400

    My listings: show pending interest badge + realtime refresh

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -444,7 +444,7 @@ export default function ItemDetailPage() {
                         type="button"
                         onClick={() => {
                           setMenuOpen(false);
-                          router.push(`/edit/${item.id}`);
+                          router.push(`/edit/${item.id}/edit`);
                         }}
                       >
                         Edit post
